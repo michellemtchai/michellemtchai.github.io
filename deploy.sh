5aedd2827b2a599cbcaa7dc613bbe7446f3c9e47
michellemtchai <michelle.chai@mail.utoronto.ca>
2021-02-11 07:33:07 -0500

    add back environment that was accidentally deleted from docker-composer.yml

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,5 @@
 #! /bin/sh
 docker-compose build
-    # git config --global user.email $GITHUB_EMAIL
-    # git config --global user.name
-    # git config --global user.password $GITHUB_PASSWORD
 docker-compose run app sh -c '''
     git config --global url."https://$GITHUB_USERNAME:$GITHUB_TOKEN@github.com/".insteadOf "https://github.com/"
     yarn deploy
