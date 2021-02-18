9ea69db3a184f2543adbf918e5b5a6cfc62b3e57
michellemtchai <michelle.chai@mail.utoronto.ca>
2021-02-17 22:30:50 -0500

    fix github auth issue

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 docker-compose build
 docker-compose run app sh -c '''
     git config --global url."https://$GITHUB_USERNAME:$GITHUB_TOKEN@github.com/".insteadOf "https://github.com/"
+    git config --global user.email "$GITHUB_EMAIL"
+    git config --global user.name "$GITHUB_USERNAME"
     yarn deploy
     exit
 '''
