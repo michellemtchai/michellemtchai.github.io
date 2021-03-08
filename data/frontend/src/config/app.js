f5c3f3accb0720e623df974bd9fded58c99799fc
michellemtchai <michelle.chai@mail.utoronto.ca>
2021-03-08 04:22:33 -0500

    pass env to react app

--- data/frontend/src/config/app.js
+++ data/frontend/src/config/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { withRouter } from "react-router";
+import { withRouter } from 'react-router';
 import Template from '../components/template/template';
 
 import { Switch, Route } from 'react-router-dom';
@@ -7,32 +7,32 @@ import { routes } from '../config/routes';
 import { fetchAll } from './api';
 
 class App extends React.Component {
-    route = (key, i)=>{
+    route = (key, i) => {
         let Component = withRouter(routes[key].component);
-        let pageTemplate = ()=>(
+        let pageTemplate = () => (
             <Template {...this.props}>
-                <Component {...this.props}/>
+                <Component {...this.props} />
             </Template>
         );
-        return (<Route key={'route-'+i}
-            exact={routes[key].exact? true: false}
-            path={key}
-            component={pageTemplate}
-        />);
-    }
+        return (
+            <Route
+                key={'route-' + i}
+                exact={routes[key].exact ? true : false}
+                path={key}
+                component={pageTemplate}
+            />
+        );
+    };
 
-    componentDidMount(){
+    componentDidMount() {
         fetchAll(this.props);
     }
 
     render() {
-        let data = this.props.state.data;
-        return(
-            <div className='content'>
+        return (
+            <div className="content">
                 <Switch>
-                    {Object.keys(routes).map((key,i)=>
-                        this.route(key,i)
-                    )}
+                    {Object.keys(routes).map((key, i) => this.route(key, i))}
                 </Switch>
             </div>
         );
--- data/frontend/src/shared/network.js
+++ data/frontend/src/shared/network.js
@@ -1,125 +1,141 @@
-export const baseUrl = process.env.DATA_BACKEND_ENV == 'development' ?
-	`http://localhost:${process.env.DATA_BACKEND_PORT}`: '';
+export const baseUrl =
+	process.env.REACT_APP_ENV == 'development'
+		? `http://localhost:${process.env.REACT_APP_PORT}`
+		: '';
 
-export const fetchConfig = (props, setStateFn, {
-		method = 'GET', params = {},
-		formatData = data=>data, minStored = 0,
+export const fetchConfig = (
+	props,
+	setStateFn,
+	{
+		method = 'GET',
+		params = {},
+		formatData = (data) => data,
+		minStored = 0,
 		next = null,
-	}={})=>{
+	} = {}
+) => {
 	return {
-	    setState: setStateFn,
-	    setError: props.setError,
-	    fetching: props.startFetching,
-	    formatData: formatData,
-	    method: method,
-	    params: params,
-	    minStored: minStored,
-	    next: next ? next : (err)=>{},
+		setState: setStateFn,
+		setError: props.setError,
+		fetching: props.startFetching,
+		formatData: formatData,
+		method: method,
+		params: params,
+		minStored: minStored,
+		next: next ? next : (err) => {},
 	};
-}
+};
 
-export const fetchAPIData = (props, url, setStateFn, {
-        method, params, formatData, minStored, next
-    }={})=>{
-    fetchData(baseUrl+url, fetchConfig(props, setStateFn, {
-    	method: method,
-    	params: params,
-        formatData: formatData,
-        minStored: minStored,
-        next: next,
-    }));
-}
+export const fetchAPIData = (
+	props,
+	url,
+	setStateFn,
+	{ method, params, formatData, minStored, next } = {}
+) => {
+	fetchData(
+		baseUrl + url,
+		fetchConfig(props, setStateFn, {
+			method: method,
+			params: params,
+			formatData: formatData,
+			minStored: minStored,
+			next: next,
+		})
+	);
+};
 
-export const fetchData = (url, config)=>{
+export const fetchData = (url, config) => {
 	url = setUpURL(url, config);
 	let storageName = `${config.method} ${url}`;
 	const cacheData = localStorage.getItem(storageName);
-	if(cacheData){
+	if (cacheData) {
 		let data = JSON.parse(cacheData);
-		if(config.minStored == 0||
-			timeDiffMinutes(data.date) > config.minStored){
+		if (
+			config.minStored == 0 ||
+			timeDiffMinutes(data.date) > config.minStored
+		) {
 			localStorage.removeItem(storageName);
 			fetchData(url, config);
-		}
-		else{
+		} else {
 			config.setState(config.formatData(data.data));
 			config.next(false);
 		}
-	}
-	else{
+	} else {
 		config.fetching();
 		let error = false;
 		fetch(url, fetchInterface(config))
-		.then(res=>{
-			if(res.status != 200){
-				if(res.status == 404){
-					error = true;
-					return res.json();
+			.then((res) => {
+				if (res.status != 200) {
+					if (res.status == 404) {
+						error = true;
+						return res.json();
+					}
+					throw Error(res.statusText);
 				}
-				throw Error(res.statusText);
-			}
-			return res.json();
-		})
-		.then(data=>{
-			if(!error){
-				localStorage.setItem(storageName, JSON.stringify({
-					date: Date.now(),
-					data: data,
-				}));
-				config.setState(config.formatData(data));
-				config.next(false);
-			}
-			else{
-				config.setError(data.msg);
+				return res.json();
+			})
+			.then((data) => {
+				if (!error) {
+					localStorage.setItem(
+						storageName,
+						JSON.stringify({
+							date: Date.now(),
+							data: data,
+						})
+					);
+					config.setState(config.formatData(data));
+					config.next(false);
+				} else {
+					config.setError(data.msg);
+					config.next(true);
+				}
+			})
+			.catch((error) => {
+				config.setError(error.message);
 				config.next(true);
-			}
-		})
-		.catch(error=>{
-			config.setError(error.message);
-			config.next(true);
-	    });
+			});
 	}
-}
+};
 
-const setUpURL = (url, config)=>{
-	if(config.method == 'GET' &&
-		Object.keys(config.params).length > 0){
-		url +=`?${paramsToQueryString(config.params)}`;
+const setUpURL = (url, config) => {
+	if (config.method == 'GET' && Object.keys(config.params).length > 0) {
+		url += `?${paramsToQueryString(config.params)}`;
 	}
 	return url;
-}
+};
 
-const fetchInterface = (config)=>{
+const fetchInterface = (config) => {
 	let data = {
-    	method: config.method,
-	    mode: 'cors',
-	    cache: 'no-cache',
-	    credentials: 'same-origin',
-	    headers: {
-	      'Content-Type': 'application/json'
-	    },
-	    referrerPolicy: 'no-referrer'
+		method: config.method,
+		mode: 'cors',
+		cache: 'no-cache',
+		credentials: 'same-origin',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		referrerPolicy: 'no-referrer',
 	};
-	if(config.method != 'GET'){
+	if (config.method != 'GET') {
 		data['body'] = JSON.stringify(config.params);
 	}
 	return data;
-}
+};
 
-export const paramsToQueryString = (params) =>{
+export const paramsToQueryString = (params) => {
 	return Object.keys(params)
-		.map(key =>
-			`${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
+		.map(
+			(key) =>
+				`${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
 		)
 		.join('&');
-}
+};
 
 const oneSecond = 1000;
-const oneMinute = 60*oneSecond;
-const oneHour = 60*oneMinute;
-const oneDay = 24*oneHour;
+const oneMinute = 60 * oneSecond;
+const oneHour = 60 * oneMinute;
+const oneDay = 24 * oneHour;
 
-export const timeDiffMinutes = (date)=>{
-	let diffMs = (new Date() - date);
+export const timeDiffMinutes = (date) => {
+	let diffMs = new Date() - date;
 	return Math.round(((diffMs % oneDay) % oneHour) / oneMinute);
-}
+};
