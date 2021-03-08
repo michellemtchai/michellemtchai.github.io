31712fe314e22878860ad8d595bc7e2db5e6f5f0
michellemtchai <michelle.chai@mail.utoronto.ca>
2021-03-07 22:13:25 -0500

    remove copy gitignore line

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,6 @@ docker-compose run app sh -c '''
     git config --global url."https://$GITHUB_USERNAME:$GITHUB_TOKEN@github.com/".insteadOf "https://github.com/"
     git config --global user.email "$GITHUB_EMAIL"
     git config --global user.name "$GITHUB_USERNAME"
-    cp .gitignore ./build/.gitignore
     yarn deploy
     exit
 '''
