cb9229b7aab159c85de340130b030871cb7386c4
michellemtchai <michelle.chai@mail.utoronto.ca>
2021-03-19 01:43:22 -0400

    add styling for tab link

--- data/frontend/src/components/project/tabLink.js
+++ data/frontend/src/components/project/tabLink.js
@@ -3,7 +3,12 @@ import React from 'react';
 class TabLink extends React.Component {
     render() {
         return (
-            <a href={this.props.link} target="_blank">
+            <a
+                href={this.props.link}
+                target="_blank"
+                className="tab-link"
+                title={this.props.link}
+            >
                 {this.props.link}
             </a>
         );
