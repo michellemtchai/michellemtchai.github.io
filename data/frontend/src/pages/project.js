ce33df2de6a7382de55805ed3391f7e8fd12f20e
michellemtchai <michelle.chai@mail.utoronto.ca>
2021-03-18 23:22:10 -0400

    add more setup to project page

--- data/frontend/src/pages/project.js
+++ data/frontend/src/pages/project.js
@@ -8,9 +8,11 @@ class Project extends React.Component {
     project = this.props.state.data.projects[
         this.props.match.params.project
     ];
+
     editProject = () => {
         goToPage(`/projects/${this.project._id}/edit`);
     };
+
     deleteProject = () => {
         let text = 'Are you sure you want to delete this project?';
         if (confirm(text)) {
@@ -25,6 +27,7 @@ class Project extends React.Component {
             );
         }
     };
+
     render() {
         let project = this.project;
         return project ? (
@@ -34,14 +37,34 @@ class Project extends React.Component {
                         src={project.image_url}
                         alt={project.name}
                     />
-                    <h2>{project.name}</h2>
+                </section>
+                <section>
+                    <h2>
+                        {project.name}
+                        <Demo url={project.demo_url} />
+                    </h2>
                     <button onClick={this.editProject}>
                         Edit Project
                     </button>
                     <button onClick={this.deleteProject}>
                         Delete Project
                     </button>
+                    <h3>Summary: </h3>
                     <p>{project.summary}</p>
+                    <ul>
+                        <li>
+                            <b>Source: </b>
+                            <TabLink link={project.source_url} />
+                        </li>
+                        {project.demo_url ? (
+                            <li>
+                                <b>Demo: </b>
+                                <TabLink link={project.demo_url} />
+                            </li>
+                        ) : (
+                            ''
+                        )}
+                    </ul>
                 </section>
                 <section>{project.description}</section>
             </article>
@@ -52,3 +75,15 @@ class Project extends React.Component {
 }
 
 export default Project;
+
+const TabLink = (props) => {
+    return (
+        <a href={props.link} target="_blank">
+            {props.link}
+        </a>
+    );
+};
+
+const Demo = (props) => {
+    return props.url ? <span>Demo</span> : '';
+};
