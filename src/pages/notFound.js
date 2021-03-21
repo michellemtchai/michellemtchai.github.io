d95fbc9e20cf97fd1e18635f0e36c574197cb688
michellemtchai <michelle.chai@mail.utoronto.ca>
2021-03-21 09:22:51 -0400

    finish page not found

--- src/pages/notFound.js
+++ src/pages/notFound.js
@@ -2,8 +2,30 @@ import React from 'react';
 
 class NotFound extends React.Component {
     render() {
-        return <div>Not Found</div>;
+        return (
+            <div className="page-body" style={divStyle}>
+                <i
+                    className="far fa-dizzy"
+                    style={iconStyle}
+                ></i>
+                <br />
+                <p style={textStyle}>Sorry, page not found.</p>
+            </div>
+        );
     }
 }
 
 export default NotFound;
+
+const divStyle = {
+    textAlign: 'center',
+    fontSize: '50px',
+    paddingTop: '50px',
+    lineHeight: '50px',
+};
+const iconStyle = {
+    fontSize: '150px',
+};
+const textStyle = {
+    margin: '10px',
+};
