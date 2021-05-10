f985d7e1dd78a4a42a0452ff7d6071f504d7c60f
michellemtchai <michelle.chai@mail.utoronto.ca>
2021-05-10 02:55:54 -0400

    update cors setup

--- data/config/initialize.js
+++ data/config/initialize.js
@@ -6,9 +6,24 @@ module.exports = (app) => {
     // use gzip compression
     app.router.use(compression());
 
-    if (process.env.DATA_BACKEND_ENV == 'development') {
+    if (process.env.DATA_BACKEND_ENV === 'development') {
         // allow cross-origin requests
-        app.router.use(cors());
+        app.router.use(
+            cors({
+                optionsSuccessStatus: 200,
+            })
+        );
+    } else {
+        // use domain defined in ENV
+        let acceptedOrigins = process.env.APP_ACCEPTED_ORIGINS.split(
+            ','
+        );
+        app.router.use(
+            cors({
+                origin: acceptedOrigins,
+                optionsSuccessStatus: 200,
+            })
+        );
     }
     // parse request body
     app.router.use(
