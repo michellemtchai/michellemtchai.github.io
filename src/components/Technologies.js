dcd4d415d0648a469f1a4648b541f0eed450b653
michellemtchai <michelle.chai@mail.utoronto.ca>
2021-09-05 12:28:55 -0400

    style tech stack

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ExternalLink from './ExternalLink';
 import Image from './Image';
+import * as styles from './Technologies.module.scss';
 
 const Technologies = ({ list }) => {
     return (
-        <ul>
+        <ul className={styles.node}>
             {list.map((tech) => (
                 <li key={tech.contentful_id}>
-                    <Image src={tech.icon} alt={tech.name} />
                     {tech.url ? (
-                        <ExternalLink to={tech.url}>{tech.name}</ExternalLink>
+                        <ExternalLink to={tech.url}>
+                            <Image src={tech.icon} alt={tech.name} />{' '}
+                            {tech.name}
+                        </ExternalLink>
                     ) : (
                         tech.name
                     )}
