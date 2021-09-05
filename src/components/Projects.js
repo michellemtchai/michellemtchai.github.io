5f20765ebecfba7e969b26acf575654916a7f095
michellemtchai <michelle.chai@mail.utoronto.ca>
2021-09-05 12:54:04 -0400

    style category pages

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,30 +2,37 @@ import React from 'react';
 import Image from './Image';
 import Technologies from './Technologies';
 import { Link } from 'gatsby';
+import * as styles from './Projects.module.scss';
 
 const Projects = ({ list }) => {
-    return list.map((project) => (
-        <section key={project.contentful_id}>
-            <Link to={`/projects/${project.slug}`} alt={project.name}>
-                <Image
-                    src={project.previewImage}
-                    alt={project.name}
-                    userPlaceHolder={true}
-                />
-                <ul>
-                    <li>
-                        <h3>{project.name}</h3>
-                    </li>
-                    <li>
-                        <p>{project.summary}</p>
-                    </li>
-                    <li>
-                        <Technologies list={project.technologies} />
-                    </li>
-                </ul>
-            </Link>
-        </section>
-    ));
+    return (
+        <div className={styles.projects}>
+            {list.map((project) => (
+                <section key={project.contentful_id}>
+                    <Link to={`/projects/${project.slug}`} alt={project.name}>
+                        <figure className={styles.previewImage}>
+                            <Image
+                                src={project.previewImage}
+                                alt={project.name}
+                                userPlaceHolder={true}
+                            />
+                        </figure>
+                        <ul>
+                            <li>
+                                <h3>{project.name}</h3>
+                            </li>
+                            <li>
+                                <p>{project.summary}</p>
+                            </li>
+                            <li>
+                                <Technologies list={project.technologies} />
+                            </li>
+                        </ul>
+                    </Link>
+                </section>
+            ))}
+        </div>
+    );
 };
 
 export default Projects;
