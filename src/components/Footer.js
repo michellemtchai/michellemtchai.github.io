86a8d8ed3273b59f4f03d9a54c410b00fe437421
michellemtchai <michelle.chai@mail.utoronto.ca>
2021-09-07 05:00:19 -0400

    style footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import * as styles from './Footer.module.scss';
 
 const Footer = () => {
-    return (
-        <footer>
-            <p>Michelle Chai &#169; 2021</p>
-        </footer>
-    );
+	return (
+		<footer className={styles.footer}>
+			<p>Michelle Chai &#169; 2021</p>
+		</footer>
+	);
 };
 
 export default Footer;
