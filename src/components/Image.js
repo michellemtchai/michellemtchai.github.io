bf4ca8f0016a6c9a7e613ac617dc62573d356217
michellemtchai <michelle.chai@mail.utoronto.ca>
2021-09-06 21:46:22 -0400

    set images to fit container

--- src/components/Image.js
+++ src/components/Image.js
@@ -9,7 +9,7 @@ const Image = ({ src, alt, className, userPlaceHolder = false }) => {
                 className={className}
                 image={getImage(src)}
                 alt={alt}
-                imgStyle={{ objectFit: 'none', objectPosition: '50% 50%' }}
+                imgStyle={{ objectFit: 'contain', objectPosition: '50% 50%' }}
             />
         );
     } else {
