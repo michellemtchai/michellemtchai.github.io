ec68d80db654ce0e58fb35c81fe6de734b99d58d
michellemtchai <michelle.chai@mail.utoronto.ca>
2021-09-14 21:08:57 -0400

    update navbar/nav modal mechanism

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,25 +4,33 @@ import Header from './Header';
 import NavBar from './NavBar';
 import Footer from './Footer';
 import Modal from './Modal';
-// import { useViewPort } from '../hooks/viewPort';
+import { useViewPort } from '../hooks/viewPort';
 import '../styles/index.scss';
 import * as styles from './Layout.module.scss';
 
 const Layout = ({ children, title, description, category }) => {
 	const [minimized, updateMinimized] = useState(false);
+	const { width } = useViewPort();
 	return (
 		<div className={styles.layout}>
 			<Seo pageTitle={title} pageDescription={description} lang="en" />
 			<Header minimized={minimized} updateMinimized={updateMinimized} />
 			<div className={styles.nonHeader}>
-				<Modal
-					className={styles.navModal}
-					show={minimized}
-					updateShow={updateMinimized}
-				>
-					<NavBar selected={category} />
-				</Modal>
-				<NavBar selected={category} minimized={minimized} />
+				{width <= 1200 && (
+					<Modal
+						className={styles.navModal}
+						show={minimized}
+						updateShow={updateMinimized}
+					>
+						<NavBar selected={category} minimized={false} />
+					</Modal>
+				)}
+				{width > 800 && (
+					<NavBar
+						selected={category}
+						minimized={width > 1200 ? minimized : true}
+					/>
+				)}
 				<main>
 					<div className={styles.pageBody}>
 						<article>{children}</article>
