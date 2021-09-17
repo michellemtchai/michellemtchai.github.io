e13cac2fc83f8ec785b87ae094f9cf21459a89f6
michellemtchai <michelle.chai@mail.utoronto.ca>
2021-09-17 02:39:06 -0400

    update searchbar style and remove unused import in index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Layout from '../components/Layout';
 import CategoryPreview from '../components/CategoryPreview';
 import SearchBar from '../components/SearchBar';
