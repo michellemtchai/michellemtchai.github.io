f43337d1ae8e519bfc5d397256f82ca7f2ccaa0c
michellemtchai <michelle.chai@mail.utoronto.ca>
2021-09-18 23:55:31 -0400

    create search page

---
+++ src/pages/search.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import SearchBar from '../components/SearchBar';
+import { graphql } from 'gatsby';
+
+export const query = graphql`
+	query {
+		allContentfulProject {
+			nodes {
+				contentful_id
+				slug
+				name
+				demoUrl
+				previewImage {
+					gatsbyImageData(
+						width: 450
+						placeholder: BLURRED
+						formats: [AUTO, WEBP]
+					)
+				}
+			}
+		}
+	}
+`;
+const Search = (props) => {
+	console.log(props);
+	return (
+		<Layout>
+			<SearchBar />
+			<h1>Search Page</h1>
+		</Layout>
+	);
+};
+
+export default Search;
