610063660bb2925a0273faf5724e3a750a4e9861
michellemtchai <michelle.chai@mail.utoronto.ca>
2021-09-25 23:52:43 -0400

    show projects on search page

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,8 +1,56 @@
 import React from 'react';
 import Layout from './Layout';
 import SearchBar from './SearchBar';
+import Projects from './Projects';
+import { useStaticQuery, graphql } from 'gatsby';
 
 const SearchResults = ({ category, query, page }) => {
+	const { allContentfulProject } = useStaticQuery(
+		graphql`
+			query {
+				allContentfulProject {
+					nodes {
+						contentful_id
+						slug
+						name
+						summary
+						demoUrl
+						previewImage {
+							gatsbyImageData(
+								width: 320
+								placeholder: BLURRED
+								formats: [AUTO, WEBP]
+							)
+						}
+						technologies {
+							contentful_id
+							name
+							icon {
+								gatsbyImageData(
+									width: 20
+									placeholder: BLURRED
+									formats: [AUTO, WEBP]
+								)
+							}
+						}
+						category {
+							slug
+						}
+					}
+				}
+			}
+		`
+	);
+	const filteredProjects = () => {
+		if (category === 'all') {
+			return allContentfulProject.nodes;
+		} else {
+			return allContentfulProject.nodes.filter((project) => {
+				const categories = project.category.map((i) => i.slug);
+				return categories.includes(category);
+			});
+		}
+	};
 	return (
 		<Layout>
 			<SearchBar range={category} />
@@ -10,6 +58,7 @@ const SearchResults = ({ category, query, page }) => {
 			<p>category: {category}</p>
 			<p>query: {query}</p>
 			<p>page: {page}</p>
+			{allContentfulProject && <Projects list={filteredProjects()} />}
 		</Layout>
 	);
 };
