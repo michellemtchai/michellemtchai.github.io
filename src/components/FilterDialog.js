792cbab658ef67a529d568eb0412dd966f291828
michellemtchai <michellemtchai@gmail.com>
2021-10-04 05:33:56 -0400

    add slider icon to dialog heading

--- src/components/FilterDialog.js
+++ src/components/FilterDialog.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const FilterButton = ({ closeModal }) => {
     return (
         <dialog open>
-            <h1>Filter Dialog</h1>
+            <h1>
+                <FontAwesomeIcon icon={['fa', 'sliders-h']} />
+                Filter Dialog
+            </h1>
             <button onClick={closeModal}>&times;</button>
             <p>hello world</p>
         </dialog>
