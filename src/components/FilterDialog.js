77a6014559b8a267b9362de7c55f950a4932d914
michellemtchai <michellemtchai@gmail.com>
2021-10-04 07:35:46 -0400

    style filter dialog

--- src/components/FilterDialog.js
+++ src/components/FilterDialog.js
@@ -1,29 +1,34 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as styles from './FilterDialog.module.scss';
 
-const FilterButton = ({ closeModal }) => {
+const FilterDialog = ({ closeModal }) => {
     const onSubmit = () => {
         //TODO
         closeModal();
     };
     return (
-        <dialog open>
-            <section>
+        <dialog open className={styles.dialog}>
+            <section className={styles.header}>
                 <h1>
                     <FontAwesomeIcon icon={['fa', 'sliders-h']} />
                     Filter Dialog
                 </h1>
                 <button onClick={closeModal}>&times;</button>
             </section>
-            <section>
+            <section className={styles.body}>
                 <p>hello world</p>
             </section>
-            <section>
-                <button onClick={closeModal}>Cancel</button>
-                <button onClick={onSubmit}>Filter</button>
+            <section className={styles.footer}>
+                <button className={styles.cancel} onClick={closeModal}>
+                    Cancel
+                </button>
+                <button className={styles.submit} onClick={onSubmit}>
+                    Filter
+                </button>
             </section>
         </dialog>
     );
 };
 
-export default FilterButton;
+export default FilterDialog;
