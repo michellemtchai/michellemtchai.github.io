fbd3a2749e8ac4a375ed991078286a213439a322
michellemtchai <michellemtchai@gmail.com>
2021-10-04 05:36:34 -0400

    add sections to filter dialog

--- src/components/FilterDialog.js
+++ src/components/FilterDialog.js
@@ -2,14 +2,26 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const FilterButton = ({ closeModal }) => {
+    const onSubmit = () => {
+        //TODO
+        closeModal();
+    };
     return (
         <dialog open>
-            <h1>
-                <FontAwesomeIcon icon={['fa', 'sliders-h']} />
-                Filter Dialog
-            </h1>
-            <button onClick={closeModal}>&times;</button>
-            <p>hello world</p>
+            <section>
+                <h1>
+                    <FontAwesomeIcon icon={['fa', 'sliders-h']} />
+                    Filter Dialog
+                </h1>
+                <button onClick={closeModal}>&times;</button>
+            </section>
+            <section>
+                <p>hello world</p>
+            </section>
+            <section>
+                <button onClick={closeModal}>Cancel</button>
+                <button onClick={onSubmit}>Filter</button>
+            </section>
         </dialog>
     );
 };
