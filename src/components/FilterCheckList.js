5f099cd3f1dc256819b11fe75427c450edf1d97c
michellemtchai <michellemtchai@gmail.com>
2021-10-05 08:41:55 -0400

    setup state for filter check list

--- src/components/FilterCheckList.js
+++ src/components/FilterCheckList.js
@@ -1,11 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const FilterCheckList = ({ label, value, options }) => {
-    const [selected, updateSelected] = useState(value);
+    const [selectAll, updateSelectAll] = useState(value === 'all');
+    const selectValues = () => {
+        return selectAll ? options.map((i) => i.value) : [];
+    };
+    const [selected, updateSelected] = useState(selectValues());
+    const onSelectAll = () => {
+        updateSelectAll(!selectAll);
+        updateSelected(selectValues());
+    };
     const onChange = (event) => {
         const newSelected = event.target.value;
-        updateSelected(newSelected);
+        const selectedCopy = [...selected];
+        if (selected.includes(newSelected)) {
+            const index = selectedCopy.indexOf(newSelected);
+            if (index !== -1) {
+                selectedCopy.splice(index, 1);
+            }
+        } else {
+            selectedCopy.push(newSelected);
+        }
+        updateSelected(selectedCopy);
     };
+    useEffect(() => {
+        updateSelectAll(selected.length === options.length);
+    }, [selected]);
     return (
         <fieldset>
             <section>
@@ -16,6 +36,8 @@ const FilterCheckList = ({ label, value, options }) => {
                         id="check-all"
                         name="check-all"
                         value="all"
+                        defaultChecked={selectAll}
+                        onChange={onSelectAll}
                     />
                     All
                 </label>
@@ -28,6 +50,8 @@ const FilterCheckList = ({ label, value, options }) => {
                             id={option.value}
                             name={option.value}
                             value={option.value}
+                            defaultChecked={selected.includes(option.value)}
+                            onChange={onChange}
                         />
                         {option.label}
                     </label>
