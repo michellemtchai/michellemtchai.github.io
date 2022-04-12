a893ad786a9453d1b6a5cd22be2bb7822f61bb23
michellemtchai <michelle.chai@mail.utoronto.ca>
2022-04-12 15:09:18 -0400

    update deploy setup

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 docker-compose build app
 docker-compose run app sh -c '''
+    git remote set-url origin $GITHUB_REPO
     git config --global url."https://$GITHUB_USERNAME:$GITHUB_TOKEN@github.com/".insteadOf "https://github.com/"
     git config --global user.email "$GITHUB_EMAIL"
     git config --global user.name "$GITHUB_USERNAME"
